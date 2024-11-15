ecc5d8093fc12965d9a3316cf2f3cf4fd8738f09
Kasyan Roman <kasanr223@gmail.com>
2024-11-15 21:31:04 +0200

    lll

--- poiu.py
+++ poiu.py
@@ -3,5 +3,12 @@ class person:
     age =12
     def get(self):
         print(self.name,self.age)
+
+    def checkAge(self):
+        if self.age > 18:
+            print('Повнолітня людина')
+
+        else:
+            print('Error')
 a=person()
 a.get()
